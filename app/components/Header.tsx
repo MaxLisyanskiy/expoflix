bf798ea14df060240fffac766378fdc40dc2f1e3
Maksim Lisianskii <Max_Lisyanskiy@mail.ru>
2023-03-01 22:37:17 +0300

    feat: add babel, prettier and alies

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+class Header extends Component {
+	render() {
+		return (
+			<Text>Open up App.tsx to start working on your app!</Text>
+		)
+	}
+}
+
+export default Header
